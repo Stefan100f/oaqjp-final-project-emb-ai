22cbdb879633ada1dbf18921546142774f15073f
Stefan100f <stefanpetrovstoev@gmail.com>
2026-06-16 11:11:45 -0400

    Version final revisada con pylint

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module provides a Flask web application for emotion detection.
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +8,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
+    """Renders the index page."""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
+    """Analyzes the emotion of the given text and returns the result."""
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
 
     if result['dominant_emotion'] is None:
-        return "Invalid text! Please try again."
+        return "Invalid text! Please try again!"
 
     anger = result['anger']
     disgust = result['disgust']
